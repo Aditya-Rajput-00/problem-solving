6ea280e30bd6191a6ed94102a6bbd770ea83b9e5
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-07-10 21:16:43 +0530

    Add HackerRank Easy: Arithmetic Operators

---
+++ HackerRank/Easy/Arithmetic-Operators.py
@@ -0,0 +1,19 @@
+"""
+Problem: Arithmetic Operators
+Platform: HackerRank
+Difficulty: Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/python-arithmetic-operators
+
+Summary:
+Read two integers and print their sum, difference, and product.
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
