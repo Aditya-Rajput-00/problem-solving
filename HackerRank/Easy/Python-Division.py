ade4600d09e365d0542e51dc175d7c8929f12f5e
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-07-10 21:20:37 +0530

    Add HackerRank Easy: Python Division

---
+++ HackerRank/Easy/Python-Division.py
@@ -0,0 +1,17 @@
+"""
+Problem: Python: Division
+Platform: HackerRank
+Difficulty: Easy
+
+Problem Link:
+https://www.hackerrank.com/challenges/python-division
+
+Summary:
+Read two integers and print the result of integer division and float division.
+"""
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
