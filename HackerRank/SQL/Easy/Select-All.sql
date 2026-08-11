c09b174786f3f930e988cc03e2354c19f253bc1c
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-08-11 09:15:02 +0530

    Add HackerRank SQL Easy: Select All

---
+++ HackerRank/SQL/Easy/Select-All.sql
@@ -0,0 +1,15 @@
+/*
+Problem: Select All
+Platform: HackerRank
+Difficulty: Easy
+
+Problem:
+Query all columns from every row in the CITY table.
+
+Concepts:
+- SELECT
+- *
+*/
+
+SELECT *
+FROM CITY;
