994b7a38a9f1d4a9a696a4d6976eec975bb2d6ef
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-08-11 09:17:49 +0530

    Add HackerRank SQL Easy: Select By ID

---
+++ HackerRank/SQL/Easy/Select-By-ID.sql
@@ -0,0 +1,17 @@
+/*
+Problem: Select By ID
+Platform: HackerRank
+Difficulty: Easy
+
+Problem:
+Query all columns from the CITY table for the city
+whose ID is 1661.
+
+Concepts:
+- SELECT
+- WHERE
+*/
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
