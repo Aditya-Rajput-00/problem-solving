3789272f17a38327c1c01fbbf009cd678d626fc9
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-08-21 14:02:49 +0530

    Add HackerRank SQL Easy: Weather Observation Station 1

---
+++ HackerRank/SQL/Easy/Weather-Observation-Station-1.sql
@@ -0,0 +1,15 @@
+/*
+Problem: Weather Observation Station 1
+Platform: HackerRank
+Difficulty: Easy
+
+Problem:
+Query a list of CITY and STATE from the STATION table.
+
+Concepts:
+- SELECT
+- Selecting multiple columns
+*/
+
+SELECT CITY, STATE
+FROM STATION;
