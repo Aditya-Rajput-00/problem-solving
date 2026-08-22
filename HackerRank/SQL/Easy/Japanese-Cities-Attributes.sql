78758f21debf4095966939b13a59e6771586f0c3
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-08-22 13:10:04 +0530

    Update HackerRank SQL Easy: Japanese Cities Attributes

--- HackerRank/SQL/Easy/Japanese-Cities-Attributes.sql
+++ HackerRank/SQL/Easy/Japanese-Cities-Attributes.sql
@@ -7,6 +7,9 @@ Problem:
 Query all attributes of every Japanese city in the CITY table.
 The COUNTRYCODE for Japan is JPN.
 
+Problem Link:
+https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
 Concepts:
 - SELECT
 - WHERE
