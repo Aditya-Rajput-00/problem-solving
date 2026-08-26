4f6e97717e6d718c8a84f03cd34e952f7239236b
Aditya-Rajput-00 <adityasinghstudent2002@gmail.com>
2026-08-26 23:28:02 +0530

    Add HackerRank Employee Names SQL solution

---
+++ HackerRank/SQL/Easy/Employee-Names.sql
@@ -0,0 +1,19 @@
+/*
+Problem: Employee Names
+Platform: HackerRank
+Difficulty: Easy
+Problem Link: https://www.hackerrank.com/challenges/name-of-employees/problem
+
+Problem:
+Write a query that prints a list of employee names
+from the Employee table in alphabetical order.
+
+Concepts:
+- SELECT
+- ORDER BY
+- ASC
+*/
+
+SELECT NAME
+FROM Employee
+ORDER BY NAME ASC;
